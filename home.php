b658146503ef40a68072acbbdaecc6f32ad69968
tomeu <tomeullompartpiza@gmail.com>
2018-11-16 18:52:37 +0100

    Initial views and router script test

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<header>
+		Welcome
+	</header>
+	<nav>
+		<a href=""></a>
+		<a href=""></a>
+		<a href=""></a>
+	</nav>
+</body>
+</html>
---
+++ robots.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Robots</title>
+</head>
+<body>
+	<header>
+		<h1>Check Our Robots</h1>
+	</header>
+
+	<!--  Paint here our robots-->
+
+</body>
+</html>
